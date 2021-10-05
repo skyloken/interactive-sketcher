ff2d3f70e350957694fe6fb5c940f8e7a45fb8ab
skyloken <risora1234@gmail.com>
2021-10-05 15:43:03 +0900

    Add decoder-only Transformer

--- src/layers.py
+++ src/layers.py
@@ -287,3 +287,80 @@ class Decoder(tf.keras.layers.Layer):
 
         # x.shape == (batch_size, target_seq_len, d_model)
         return x, attention_weights
+
+
+class DecoderOnlyLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderOnlyLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(d_model, num_heads)
+
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, look_ahead_mask):
+        # enc_output.shape == (batch_size, input_seq_len, d_model)
+
+        attn1, attn_weights_block = self.mha(
+            x, x, x, look_ahead_mask
+        )  # (batch_size, target_seq_len, d_model)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(attn1 + x)
+
+        ffn_output = self.ffn(out1)  # (batch_size, target_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(
+            ffn_output + out1
+        )  # (batch_size, target_seq_len, d_model)
+
+        return out2, attn_weights_block
+
+class DecoderOnly(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        num_layers,
+        d_model,
+        num_heads,
+        dff,
+        target_vocab_size,
+        maximum_position_encoding,
+        rate=0.1,
+    ):
+        super(DecoderOnly, self).__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.embedding = tf.keras.layers.Embedding(target_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, d_model)
+
+        self.dec_layers = [
+            DecoderOnlyLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)
+        ]
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, look_ahead_mask):
+
+        seq_len = tf.shape(x)[1]
+        attention_weights = {}
+
+        x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x, block = self.dec_layers[i](
+                x, training, look_ahead_mask
+            )
+
+            attention_weights["decoder_layer{}".format(i + 1)] = block
+
+        # x.shape == (batch_size, target_seq_len, d_model)
+        return x, attention_weights
--- src/transformer.py
+++ src/transformer.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from layers import Decoder, Encoder
+from layers import Decoder, DecoderOnly, Encoder
 
 
 class Transformer(tf.keras.Model):
@@ -39,6 +39,44 @@ class Transformer(tf.keras.Model):
         # dec_output.shape == (batch_size, tar_seq_len, d_model)
         dec_output, attention_weights = self.decoder(
             tar, enc_output, training, look_ahead_mask, dec_padding_mask
+        )
+
+        final_output = self.final_layer(
+            dec_output
+        )  # (batch_size, tar_seq_len, target_vocab_size)
+
+        return final_output, attention_weights
+
+class DecoderOnlyTransformer(tf.keras.Model):
+    def __init__(
+        self,
+        num_layers,
+        d_model,
+        num_heads,
+        dff,
+        target_vocab_size,
+        pe_target,
+        rate=0.1,
+    ):
+        super(DecoderOnlyTransformer, self).__init__()
+
+        self.decoder = DecoderOnly(
+            num_layers, d_model, num_heads, dff, target_vocab_size, pe_target, rate
+        )
+
+        self.final_layer = tf.keras.layers.Dense(target_vocab_size)
+
+    def call(
+        self, tar, training, look_ahead_mask
+    ):
+
+        # enc_output = self.encoder(
+        #     inp, training, enc_padding_mask
+        # )  # (batch_size, inp_seq_len, d_model)
+
+        # dec_output.shape == (batch_size, tar_seq_len, d_model)
+        dec_output, attention_weights = self.decoder(
+            tar, training, look_ahead_mask,
         )
 
         final_output = self.final_layer(
