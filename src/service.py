23c53e071427309ee444245fea2ba3458070972d
Kenta Tanigaki <tanigaki.kit@gmail.com>
2022-03-24 21:22:49 +0900

    Fix

--- src/service.py
+++ src/service.py
@@ -43,7 +43,7 @@ class Agent:
         ckpt = tf.train.Checkpoint(epoch=tf.Variable(1),
                                    transformer=self.interactive_sketcher)
 
-        checkpoint_path = "../models/isketcher/model_12/checkpoints_3000/"
+        checkpoint_path = "../model"
         ckpt_manager = tf.train.CheckpointManager(
             ckpt, checkpoint_path, max_to_keep=5)
 
